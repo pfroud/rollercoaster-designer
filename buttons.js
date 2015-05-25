9c259f9650da30c1ee98f5887930dd3abe3933fb
Peter Froud <pfroud@ucsc.edu>
2015-05-25 16:47:09 -0700

    Fixed more merge errors.

--- buttons.js
+++ buttons.js
@@ -1,48 +1,6 @@
 "use strict";
 
-window.onload = function () {
 
-    document.getElementById("flat").onclick = function () {
-        pieces = [slope.flat];
-        addPieces();
-    };
-
-    document.getElementById("delete").onclick = function () {
-        scene.remove(allTracks.pop());
-        scene.remove(allBoundingBoxes.pop());
-    };
-
-    document.getElementById("flatToUp").onclick = function () {
-        pieces = [slope.flatToUp];
-        addPieces();
-    };
-
-    document.getElementById("up").onclick = function () {
-        pieces = [slope.up];
-        addPieces();
-    };
-
-    document.getElementById("upToFlat").onclick = function () {
-        pieces = [slope.upToFlat];
-        addPieces();
-    };
-
-    document.getElementById("flatToDown").onclick = function () {
-        pieces = [slope.flatToDown];
-        addPieces();
-    };
-
-    document.getElementById("down").onclick = function () {
-        pieces = [slope.down];
-        addPieces();
-    };
-
-    document.getElementById("downToFlat").onclick = function () {
-        pieces = [slope.downToFlat];
-        addPieces();
-    };
-};
-=======
 // dat.gui object (menu in the top)
 var pieceAdder = new dat.GUI();
 
@@ -81,4 +39,3 @@ function addPiece(piece){
     pieces = [piece];
     addPieces();
 }
->>>>>>> jonathans_branch
