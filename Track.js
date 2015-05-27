503bf9d5b9c40843fad127babb57111f9904424a
jonathanBridge <jvbridge@gmail.com>
2015-05-27 12:55:18 -0700

    Cleaned up code TODO: made advance scale properly

--- Track.js
+++ Track.js
@@ -1,23 +1,41 @@
 "use strict";
 
 // GLOBAL VARIABLES ============================================================
-var i = 0;
-
-var LOADER = new THREE.JSONLoader(), SCALE = 0.01;
+var TRACK = new Track();
 
 /**
- * Track object, referred to by global variable TRACK
+ * @summary Track object, referred to by global variable TRACK
  * @constructor
+ * @type: class
  *
+ * @member pieces: list of all track pieces. Must be of class type Piece
+ * @see Piece.js
  *
+ * @member trackMeshes: list of all the meshes of the track. May also be
+ * accessed through pieces[pieceIndex].mesh
  *
+ * @private boundingBoxes: list of all the bounding boxes of the pieces. May also
+ * be accessed through pieces[pieceIndex].boundingBox
+ *
+ * @member currentX currentY currentZ: the current position in the scene of the
+ * end of the track
+ *
+ * @member prevPiece: the piece directly before the current piece. May also be
+ * accessed through pieces[pieces.length - 2]
+ *
+ * @member currPiece: the last piece on the track. May also be accessed
+ * through pieces[pieces.length -1]
+ *
+ * @jsonLoader: the jsonloader for THREE.js to access
+ *
+ * @scale: the global scale variable imported, allowed so that if we wish to
+ * overload we don't need to change the global
  */
 
 function Track() {
-    this.trackArray = [];
+    this.pieces = [];
     this.trackMeshes = [];
     this.boundingBoxes = [];
-    this.counter = 0; // OLD, may be unnecessary
     this.currentX = -1;
     this.currentY = 0;
     this.currentZ = 1;
@@ -32,18 +50,27 @@ function Track() {
 }
 
 
-
-Track.prototype.insertTrack = function(piece){
+/**
+ * Inserts the piece into the world and appends it to the track
+ * @param piece must be an object of class type Piece
+ * @see Piece.js
+ */
+Track.prototype.insertPiece = function (piece){
     if (this.currPiece != null){
-        this.prevPiece = TRACK.currPiece;
+        this.prevPiece = this.currPiece;
         this.currpiece = piece;
     } else {
         this.currPiece = piece;
     }
 
-    this.jsonLoader.load(piece.filename,
+    this.jsonLoader.load(piece.filename, /*this.createScene(geometry)*/
         function createScene(geometry) {
 
+            if (TRACK == undefined){
+                throw "ERROR: GLOBAL NOT DEFINED";
+            } else {
+                var track = TRACK;
+            }
             var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
             mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2 * TRACK.direction); //IN PROGREESS
             mesh.position.x = TRACK.currentX;
@@ -53,8 +80,6 @@ Track.prototype.insertTrack = function(piece){
             scene.add(mesh);
             
             TRACK.doPreCorrections(); //moves where the current piece will go
-
-
 
             piece.mesh = mesh;
 
@@ -70,25 +95,64 @@ Track.prototype.insertTrack = function(piece){
 };
 
 
+/**
+ * Inserts multiple pieces from an array. Works recursively
+ * @param pieces: an array of pieces
+ */
+Track.prototype.insertPieces = function(pieces){
+    if (pieces.length == 0) return;
+    this.insertPiece(pieces.pop());
+    this.insertPieces(pieces);
+};
 
+
+// UNUSED PROTOTYPE CODE
+Track.prototype.createScene = function(geometry){
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
+    mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2 * TRACK.direction); //IN PROGREESS
+    mesh.position.x = this.currentX;
+    mesh.position.y = this.currentY;
+    mesh.position.z = this.currentZ;
+    mesh.scale.set(SCALE, SCALE, SCALE);
+    scene.add(mesh);
+
+    this.doPreCorrections(); //moves where the current piece will go
+
+
+
+    piece.mesh = mesh;
+
+    this.advanceCurrent(); //moves where the next piece will go
+
+
+    this.trackMeshes.push(mesh);
+    var bbox = new THREE.BoxHelper(mesh);
+    this.boundingBoxes.push(bbox);
+    scene.add(bbox);
+};
+
+
+/**
+ * Advances currentX, CurrentY, and CurrentZ based on the new piece
+ * TODO: rotations and the Y plane
+ */
 Track.prototype.advanceCurrent = function(){
-    console.log("advance Current!");
     this.currentX += this.currPiece.size.x;
 };
 
+/**
+ * Does the precorrections necessary
+ * TODO: implement
+ */
 Track.prototype.doPreCorrections = function (){
     console.log("precorrections!");
 
 };
 
-// inserts an array of pieces
-Track.prototype.insertPieces = function (pieces){
-  for (var i = pieces.length; i > 0; i--){
-      this.insert(pieces.pop());
-  }
-};
+/**
+ * TODO: lots of testing
+ */
 
-// TODO: lots of testing
 Track.prototype.delete = function () {
     if (this.trackMeshes.length > 0){
         scene.remove(trackMeshes.pop());
@@ -122,12 +186,6 @@ Track.prototype.toggleBoxes = function(){
 
 };
 
-
-// Global variable for the track
-  var TRACK = new Track();
-
-//var tmpPiece = new Piece(TRACKTYPES.FLAT);
-//insertTrack(tmpPiece);
 
 // =============================================================================
 // TESTING GUI FOR CODE ========================================================
