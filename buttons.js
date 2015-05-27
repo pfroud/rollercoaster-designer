7ee3988f5a02a1cfa7dfce8e3a4a7bede8b4eac7
jonathanBridge <jvbridge@gmail.com>
2015-05-27 16:23:20 -0700

    added debug options

--- buttons.js
+++ buttons.js
@@ -5,16 +5,24 @@
 // TEMP GUI FOR TESTING ========================================================
 var MAIN_MENU = new dat.GUI();
 
-var debugFolder = MAIN_MENU.gui.addFolder("Debug");
-
-debugFolder.close();
-
+var debugFolder = MAIN_MENU.addFolder("Debug");
 
 var debugJSON = {
     ToggleBoxes: function(){
-
+        TRACK.toggleBoxes();
+    },
+    TogglePerspective: function(){
+        // TODO: code goes here for perspective Peter
     }
 };
+
+debugFolder.add(debugJSON, "ToggleBoxes").name("Toggle Boxes");
+debugFolder.add(debugJSON, "TogglePerspective").name("Toggle Perspective");
+
+// REMOVE THIS LINE TO KEEP DEBUG FOLDER OPEN
+debugFolder.close();
+
+
 
 
 var testingFolder = MAIN_MENU.addFolder("New Track code");
@@ -39,4 +47,4 @@ testingFolder.add(testingButtonJson,"Flat");
 testingFolder.add(testingButtonJson, "Delete");
 testingFolder.add(testingButtonJson, "DeleteAll");
 testingFolder.add(testingButtonJson, "ToggleBoxes");
-testingFolder.open();
+testingFolder.open(); // CHANGE THIS TO close() TO SET FOLDER CLOSED
