4f64a949463c7cedc3aca16776e8bd83269c4d40
Peter Froud <pfroud@ucsc.edu>
2015-06-07 19:31:14 -0700

    Trefoil knot works

--- Cart.js
+++ Cart.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 const MATERIAL_TRAIN = new THREE.MeshLambertMaterial({color: "#00ff00"});
 
@@ -7,33 +7,33 @@ var jsonLoader = new THREE.JSONLoader();
 var sc = 0.01;
 
 /*jsonLoader.load("train 3D models/3 - json/Cart_dims.json",
-    function createScene(geometry, materials) {
+ function createScene(geometry, materials) {
 
-        // create the mesh and add it to the scene
-        var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+ // create the mesh and add it to the scene
+ var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
 
-        //mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 1);
+ //mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 1);
 
-        mesh.position.x = 0;
-        mesh.position.y = 0;
-        mesh.position.z = 0;
-        mesh.scale.set(sc, sc, sc);
-        scene.add(mesh);
-
-
-        var bbox = new THREE.BoxHelper(mesh);
-        scene.add(bbox);
-    }
-);*/
+ mesh.position.x = 0;
+ mesh.position.y = 0;
+ mesh.position.z = 0;
+ mesh.scale.set(sc, sc, sc);
+ scene.add(mesh);
 
 
-var curve = new THREE.SplineCurve3( [
-    new THREE.Vector3( -10, 0, 10 ),
-    new THREE.Vector3( -5, 5, 5 ),
-    new THREE.Vector3( 0, 0, 0 ),
-    new THREE.Vector3( 5, -5, 5 ),
-    new THREE.Vector3( 10, 0, 10 )
-] );
+ var bbox = new THREE.BoxHelper(mesh);
+ scene.add(bbox);
+ }
+ );*/
+
+
+var curve = new THREE.SplineCurve3([
+    new THREE.Vector3(-10, 0, 10),
+    new THREE.Vector3(-5, 5, 5),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(5, -5, 5),
+    new THREE.Vector3(10, 0, 10)
+]);
 
 var geometry = new THREE.TubeGeometry(
     curve,  //path
@@ -42,9 +42,40 @@ var geometry = new THREE.TubeGeometry(
     8,     //radiusSegments
     false  //closed
 );
-console.log(geometry);
+/////////////////////////////////////////////////////
+THREE.Curves = {};
+THREE.Curves.TrefoilKnot = THREE.Curve.create(
+    function (s) {
+        this.scale = (s === undefined) ? 10 : s;
+    },
+    function (t) {
+        t *= Math.PI * 2;
+        var tx = (2 + Math.cos(3 * t)) * Math.cos(2 * t),
+            ty = (2 + Math.cos(3 * t)) * Math.sin(2 * t),
+            tz = Math.sin(3 * t);
+        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+    }
+);
+var extrudePath = new THREE.Curves.TrefoilKnot();
+var tube = new THREE.TubeGeometry(extrudePath, 50, 2, 10, true);
 
-var mesh = THREE.Mesh(geometry, MATERIAL_TRAIN);
-console.log(mesh);
+//scene.add(new )
 
-scene.add(mesh);
+//addGeometry(tube, 0xff00ff);
+
+
+function addGeometry(geometry, color) {
+    // 3d shape
+    tubeMesh = THREE.SceneUtils.createMultiMaterialObject(geometry, [
+        new THREE.MeshLambertMaterial({
+            color: color
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x000000,
+            opacity: 0.3,
+            wireframe: true,
+            transparent: true
+        })]);
+    scene.add(tubeMesh);
+}
+/////////////////////////////////////////////////////
